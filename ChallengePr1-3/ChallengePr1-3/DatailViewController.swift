cb50f5b8a381b1f0b64523e8b085a2213fdbfc6b
Guillermo Suarez <galleplop@gmail.com>
2023-04-26 23:03:38 -0300

    Add UIActivityViewController to share photo

--- ChallengePr1-3/ChallengePr1-3/DatailViewController.swift
+++ ChallengePr1-3/ChallengePr1-3/DatailViewController.swift
@@ -9,19 +9,39 @@ import UIKit
 
 class DatailViewController: UIViewController {
 
-    var strCountry : String = ""
+    var strCountry : String? = ""
     
     @IBOutlet var imgFlag: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        if !strCountry.isEmpty {
+        if let imageToLoad = strCountry {
             
-            imgFlag.image = UIImage(named: strCountry)
+            imgFlag.image = UIImage(named: imageToLoad)
         }
         
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+    }
+    
+    @objc func shareTapped() {
+        
+        guard let image = imgFlag.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        if let imageToLoad = strCountry {
+            
+            let acView = UIActivityViewController(activityItems: [image, imageToLoad], applicationActivities: [])
+            acView.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+            
+            present(acView, animated: true)
+        }
         
     }
 
