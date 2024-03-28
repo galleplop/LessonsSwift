b4d2f76036e1229a191e1d8120d7f0a5bb26e1df
Guillermo Suarez <galleplop@gmail.com>
2024-03-28 17:57:10 -0300

    basic UI

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var petitions = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "Title"
+        cell.detailTextLabel?.text = "Subtitle"
+        
+        return cell
+    }
 }
 
