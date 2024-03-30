03829e3eb63480b3d623c037eb2ac0ad0b91d8b4
Guillermo Suarez <galleplop@gmail.com>
2024-03-30 14:35:12 -0300

    present person info and change name on tap

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -26,6 +26,12 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         self.present(picker, animated: true)
     }
     
+    func getDocumentsDirectory() -> URL {
+        
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
     //MARK: - UICollectionViewController Delegate
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -36,9 +42,39 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else { fatalError("Unable to dequeue PersonCell") }
 
+        let person = people[indexPath.item]
         
+        cell.name.text = person.name
+        let path = getDocumentsDirectory().appendingPathExtension(person.image)
+        
+        cell.imageView.image = UIImage(contentsOfFile: path.path())
+        
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
         
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let person = people[indexPath.item]
+        
+        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+            
+            guard let newName = ac?.textFields?[0].text else { return }
+            person.name = newName
+            self?.collectionView.reloadData()
+            
+        })
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        self.present(ac, animated: true)
     }
     
     //MARK: - UIImagePickerControllerDelegate Delegate
@@ -60,14 +96,6 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         dismiss(animated: true)
     }
-    
-    func getDocumentsDirectory() -> URL {
-        
-        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return path[0]
-    }
-    
-    //MARK: - UINavigationControllerDelegate Delegate
     
 }
 
