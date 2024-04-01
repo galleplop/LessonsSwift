d37b1484d4b611f86fdc62fa7fda218d3402660a
Guillermo Suarez <galleplop@gmail.com>
2024-04-01 20:03:05 -0300

    Duplicate project10

---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Guillermo Suarez on 30/3/24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
