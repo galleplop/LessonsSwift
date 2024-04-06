8423f6f634a1a32bfe28265738f4490e0c21a9f2
Guillermo Suarez <galleplop@gmail.com>
2024-04-06 20:27:47 -0300

    add MKAnnotation

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,23 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Guillermo Suarez on 6/4/24.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+
+}
