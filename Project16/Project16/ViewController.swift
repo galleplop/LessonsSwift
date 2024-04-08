9617645c8a607f9acda826597cc703191de9c40d
Guillermo Suarez <galleplop@gmail.com>
2024-04-07 22:49:20 -0300

    Add DetailViewController with WKWebView, change pin color, add mapType

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -17,17 +17,40 @@ class ViewController: UIViewController, MKMapViewDelegate {
         // Do any additional setup after loading the view.
         title = "Some capital information"
         
-        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics")
-        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
-        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
-        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
-        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics", website: "https://en.wikipedia.org/wiki/London")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.", website: "https://en.wikipedia.org/wiki/Oslo")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.", website: "https://en.wikipedia.org/wiki/Paris")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.", website: "https://en.wikipedia.org/wiki/Rome")
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.", website: "https://en.wikipedia.org/wiki/Washington,_D.C.")
         
         mapView.addAnnotations([london, oslo, paris, rome, washington])
         
         mapView.setCenter(paris.coordinate, animated: true)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(navigationInfoPressed))
     }
-
+    
+    @objc func navigationInfoPressed() {
+        
+        let ac = UIAlertController(title: "Specify view map style", message: nil, preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Standar", style: .default) {
+            alert in
+            self.mapView.mapType = .standard
+        })
+        ac.addAction(UIAlertAction(title: "Satellite", style: .default) {
+            alert in
+            self.mapView.mapType = .satellite
+        })
+        ac.addAction(UIAlertAction(title: "Hybrid", style: .default) {
+            alert in
+            self.mapView.mapType = .hybrid
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        self.present(ac, animated: true)
+    }
+    
     //MARK: - MKMapViewDelegate implementation
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         
@@ -35,7 +58,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         let identifier = "Capital"
         
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
         
         if annotationView == nil {
             
@@ -49,6 +72,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
             annotationView?.annotation = annotation
         }
         
+        annotationView?.markerTintColor = .purple
+        
         return annotationView
     }
     
@@ -56,12 +81,19 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         guard let capital = view.annotation as? Capital else { return }
         
-        let placeName = capital.title
-        let placeInfo = capital.info
+//        let placeName = capital.title
+//        let placeInfo = capital.info
+//        
+//        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+//        ac.addAction(UIAlertAction(title: "OK", style: .default))
+//        self.present(ac, animated: true)
         
-        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        self.present(ac, animated: true)
+        if let detailViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "DetailView") as? DetailViewController {
+            
+            detailViewController.capital = capital
+            
+            navigationController?.pushViewController(detailViewController, animated: true)
+        }
     }
 }
 
