fdb7b4b5cbda862f48bc88813ba57d3a6d87a2c1
Guillermo Suarez <galleplop@gmail.com>
2024-04-11 20:19:46 -0300

    example of print function

--- Project18/Project18/ViewController.swift
+++ Project18/Project18/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("1-", terminator: "")
+        print(2, 3, 4, 5, separator: "-")
     }
 
 
