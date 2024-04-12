90e325e6b7af3636db8e4d47bcc7541f5405e515
Guillermo Suarez <galleplop@gmail.com>
2024-04-12 19:17:18 -0300

    Basic project, add files, setting up game config

---
+++ Project20/Project20/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Guillermo Suarez on 12/4/24.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+}
