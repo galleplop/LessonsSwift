680844d0010a4fc8511e611b0e6be9d819615186
Guillermo Suarez <galleplop@gmail.com>
2024-04-13 14:56:48 -0300

    add UNNotificationAction, and receive message

--- Project21/Project21/ViewController.swift
+++ Project21/Project21/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +39,8 @@ class ViewController: UIViewController {
     
     @objc func scheduleLocal() {
         
+        registerCategories()
+        
         let center = UNUserNotificationCenter.current()
         center.removeAllPendingNotificationRequests()
         
@@ -61,6 +63,41 @@ class ViewController: UIViewController {
         
         center.add(request)
     }
-
+    
+    func registerCategories() {
+        
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            
+            print("Custom data received: \(customData)")
+            
+            switch response.actionIdentifier {
+                
+            case UNNotificationDefaultActionIdentifier:
+                // the user swiped to unlock
+                print("Default identifier")
+                
+            case "show":
+                print("Show more information.")
+                
+            default:
+                break
+            }
+        }
+        
+        completionHandler()
+    }
 }
 
