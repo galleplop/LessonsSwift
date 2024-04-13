f06bd9c8eb6257fd3c08360d609877f9b3973909
Guillermo Suarez <galleplop@gmail.com>
2024-04-13 14:12:19 -0300

    basic template

---
+++ Project21/Project21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project21
+//
+//  Created by Guillermo Suarez on 13/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
