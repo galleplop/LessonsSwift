6d0eedb522ab0122837e84b9979a76fb3a7b7cfb
Guillermo Suarez <galleplop@gmail.com>
2024-04-15 19:30:50 -0300

    basic template

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Guillermo Suarez on 15/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
