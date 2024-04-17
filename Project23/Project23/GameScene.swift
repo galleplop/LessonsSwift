bdb15749ed57d0211015879dbdd341c99c6fb300
Guillermo Suarez <galleplop@gmail.com>
2024-04-16 23:43:08 -0300

    sequence and game behavior

--- Project23/Project23/GameScene.swift
+++ Project23/Project23/GameScene.swift
@@ -13,8 +13,11 @@ enum ForceBomb {
     case never, always, random
 }
 
+enum SequenceType: CaseIterable {
+    case oneNoBomb, one, twoWithOneBomb, two, three, four, chain, fastChain
+}
+
 class GameScene: SKScene {
-    
     
     var gameScore: SKLabelNode!
     var score: Int = 0 {
@@ -39,6 +42,12 @@ class GameScene: SKScene {
     
     var bombSoundEffect: AVAudioPlayer?
     
+    var popupTime = 0.9
+    var sequence = [SequenceType]()
+    var sequencePosition = 0
+    var chainDelay = 3.0
+    var nextSequenceQueued = true
+    
     override func didMove(to view: SKView) {
         
         let background = SKSpriteNode(imageNamed: "sliceBackground")
@@ -53,22 +62,46 @@ class GameScene: SKScene {
         createScore()
         createLives()
         createSlices()
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        guard let touch = touches.first else { return }
-        let location = touch.location(in: self)
-        activeSlicePoints.append(location)
-        redrawActiveSlice()
+        sequence = [.oneNoBomb, .oneNoBomb, .twoWithOneBomb, .twoWithOneBomb, .three, .one, .chain]
         
-        if !isSwooshSoundActive {
+        for _ in 0...1000 {
             
-            playSwooshSound()
+            if let nextSequence = SequenceType.allCases.randomElement() {
+                
+                sequence.append(nextSequence)
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            
+            self?.tossEnemies()
         }
     }
     
     override func update(_ currentTime: TimeInterval) {
+        
+        if activeEnemies.count > 0 {
+            
+            for (index, node) in activeEnemies.enumerated().reversed() {
+                
+                if node.position.y < -140 {
+                    
+                    node.removeFromParent()
+                    activeEnemies.remove(at: index)
+                }
+            }
+        } else {
+            
+            if !nextSequenceQueued {
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + popupTime) { [weak self] in
+                    
+                    self?.tossEnemies()
+                }
+                nextSequenceQueued = true
+            }
+        }
         
         var bombCount = 0
         
@@ -84,6 +117,19 @@ class GameScene: SKScene {
             // no bombs - stop the fuse sound
             bombSoundEffect?.stop()
             bombSoundEffect = nil
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        activeSlicePoints.append(location)
+        redrawActiveSlice()
+        
+        if !isSwooshSoundActive {
+            
+            playSwooshSound()
         }
     }
     
@@ -277,5 +323,86 @@ class GameScene: SKScene {
         
         addChild(enemy)
         activeEnemies.append(enemy)
+    }
+    
+    func tossEnemies() {
+        
+        popupTime *= 0.991
+        chainDelay *= 0.99
+        physicsWorld.speed *= 1.02
+        
+        let sequenceType = sequence[sequencePosition]
+        
+        switch sequenceType {
+            
+        case .oneNoBomb:
+            createEnemy(forceBomb: .never)
+        case .one:
+            
+            createEnemy()
+        case .twoWithOneBomb:
+            
+            createEnemy(forceBomb: .never)
+            createEnemy(forceBomb: .always)
+        case .two:
+            
+            createEnemy()
+            createEnemy()
+        case .three:
+            
+            createEnemy()
+            createEnemy()
+            createEnemy()
+        case .four:
+            
+            createEnemy()
+            createEnemy()
+            createEnemy()
+            createEnemy()
+        case .chain:
+            
+            createEnemy()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + (chainDelay / 5.0)) { [weak self] in
+                
+                self?.createEnemy()
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + (chainDelay / 5.0 * 2.0)) { [weak self] in
+                
+                self?.createEnemy()
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + (chainDelay / 5.0 * 3.0)) { [weak self] in
+                
+                self?.createEnemy()
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + (chainDelay / 5.0 * 4.0)) { [weak self] in
+                
+                self?.createEnemy()
+            }
+            
+        case .fastChain:
+            
+            createEnemy()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + (chainDelay / 10.0)) { [weak self] in
+                
+                self?.createEnemy()
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + (chainDelay / 10.0 * 2.0)) { [weak self] in
+                
+                self?.createEnemy()
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + (chainDelay / 10.0 * 3.0)) { [weak self] in
+                
+                self?.createEnemy()
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + (chainDelay / 10.0 * 4.0)) { [weak self] in
+                
+                self?.createEnemy()
+            }
+        }
+        
+        sequencePosition += 1
+        nextSequenceQueued = false
     }
 }
