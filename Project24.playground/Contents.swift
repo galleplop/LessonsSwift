48204294ab1479348bac43289d8a6eabcc92712f
Guillermo Suarez <galleplop@gmail.com>
2024-04-21 19:39:10 -0300

    working with strings

--- Project24.playground/Contents.swift
+++ Project24.playground/Contents.swift
@@ -32,4 +32,52 @@ for i in 0..<name.count {
 
 //it’s always better to use someString.isEmpty rather than someString.count == 0 if you’re looking for an empty string.
 
+//MARK: Working with strings
+
+
+let password = "123456"
+password.hasPrefix("123")
+password.hasSuffix("456")
+
+extension String {
+    
+    func deletingPrefix(_ prefix: String) -> String {
+        
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+    
+    func deletingSuffix(_ suffix: String) -> String {
+        
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
+}
+
+let testDeleting = password.deletingPrefix("123").deletingSuffix("456")
+
+//MARK: -
+
+let weather = "it's going to rain"
+print(weather.capitalized)
+
+extension String {
+    
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+print(weather.capitalizedFirst)
+
+//MARK: -
+
+let input = "Swift is like Objective-C without the C"
+input.contains("Swift")
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+languages.contains(where: input.contains) //Lika a map function. every elementy will run the closure input.contains, when one of these gets true it stops there.
 
