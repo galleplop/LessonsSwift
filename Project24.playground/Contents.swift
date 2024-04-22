fb0f048e2afa35a7d17f7f4acf8784dd5c3452f6
Guillermo Suarez <galleplop@gmail.com>
2024-04-22 13:21:19 -0300

    Formatting strings with NSAttributedString

--- Project24.playground/Contents.swift
+++ Project24.playground/Contents.swift
@@ -81,3 +81,26 @@ languages.contains("Swift")
 
 languages.contains(where: input.contains) //Lika a map function. every elementy will run the closure input.contains, when one of these gets true it stops there.
 
+
+//MARK: Formatting strings with NSAttributedString
+
+let string = "This is a test string"
+let attributes: [NSAttributedString.Key: Any] = [
+    .foregroundColor: UIColor.white,
+    .backgroundColor: UIColor.red,
+    .font: UIFont.boldSystemFont(ofSize: 36)
+]
+
+let attributedStringExample = NSAttributedString(string: string, attributes: attributes)
+
+let attributedString = NSMutableAttributedString(string: string)
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+
+//MARK: -
+
+
+
