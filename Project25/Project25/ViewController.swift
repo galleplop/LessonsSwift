18300561ed29a8db57dcf2356866f8a1378b6983
Guillermo Suarez <galleplop@gmail.com>
2024-04-22 14:56:14 -0300

    setup collectionView and imagePicker

--- Project25/Project25/ViewController.swift
+++ Project25/Project25/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    var images = [UIImage] ()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
     }
 
+    
+    @objc func importPicture() {
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        self.present(picker, animated: true)
+    }
+    
+    //MARK: - UIImagePickerControllerDelegate
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        images.insert(image, at: 0)
+        collectionView.reloadData()
+        
+        dismiss(animated: true)
+    }
+    
+    //MARK: - UICollectionViewController
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(10) as? UIImageView {
+            
+            imageView.image = images[indexPath.item]
+        }
+        
+        return cell
+    }
 }
 
