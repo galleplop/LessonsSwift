51a5b256b22b7b6345ac75b65b954c20b24d168c
Guillermo Suarez <galleplop@gmail.com>
2024-04-22 14:25:37 -0300

    new project template

---
+++ Project25/Project25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25
+//
+//  Created by Guillermo Suarez on 22/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
